aa88a93539471fe6bb693e5263acbfc5acace93c
LiamClark <L.A.Clark@student.tudelft.nl>
2017-11-23 18:34:21 +0100

    Cleanup Util test
    The @RunWith should not be necessary anymore.
    None of the tests throw exceptions.
    The default intelij file template show up as a warning and is annoying.

--- app/src/test/java/nl/tudelft/cs4160/trustchain_android/Util/UtilTest.java
+++ app/src/test/java/nl/tudelft/cs4160/trustchain_android/Util/UtilTest.java
@@ -3,46 +3,39 @@ package nl.tudelft.cs4160.trustchain_android.Util;
 import junit.framework.Assert;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 import static nl.tudelft.cs4160.trustchain_android.Util.Util.ellipsize;
-import static org.junit.Assert.*;
 
-/**
- * Created by meijer on 10-11-17.
- */
-@RunWith(JUnit4.class)
 public class UtilTest {
     @Test
-    public void ellipsizetest() throws Exception {
+    public void ellipsizetest() {
         String input = "12345678910";
         String expected = "1(..)0";
         Assert.assertEquals(expected,ellipsize(input,5));
     }
 
     @Test
-    public void ellipsizetest2() throws Exception {
+    public void ellipsizetest2() {
         String input = "12345678910";
         String expected = "12(..)10";
         Assert.assertEquals(expected,ellipsize(input,8));
     }
 
     @Test
-    public void ellipsizetest3() throws Exception {
+    public void ellipsizetest3() {
         String input = "12345678910";
         Assert.assertEquals(input,ellipsize(input,11));
     }
 
     @Test
-    public void ellipsizetest4() throws Exception {
+    public void ellipsizetest4() {
         String input = "12345678910";
         String expected = "1(..)0";
         Assert.assertEquals(expected,ellipsize(input,6));
     }
 
     @Test
-    public void ellipsizetest5() throws Exception {
+    public void ellipsizetest5() {
         String input = "12";
         String expected = "12";
         Assert.assertEquals(expected,ellipsize(input,5));
